eeb539384e656dc0c5fa5043300488efccb620ba
overkri <overkri@gmail.com>
2020-02-01 19:09:08 +0300

    BugFix

--- Assets/Scripts/TextBoxBehaviour.cs
+++ Assets/Scripts/TextBoxBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextBoxBehviour : MonoBehaviour
+public class TextBoxBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
